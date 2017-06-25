7b157b0529ba60ed0a4b074a9f1609cd9029ecb9
Huntington Shin Jasperson <huntingtonjasperson@gmail.com>
2017-06-25 14:52:18 -0700

    platform works

--- mainssrts/platform.js
+++ mainssrts/platform.js
@@ -1,5 +1,16 @@
 //platform class
-function Platform {
+function Platform() {
+  this.x = random(6000);
+  this.y = random(800);
+  this.wid = 40;
+  this.len = random(40,200);
   
-  
+  this.display = function() {
+    if (this.x <= 1200) {
+      noStroke();
+      fill(255);
+      rect(this.x,this.y,this.len,this.wid);
+    } 
+    
+  }
 };
--- mainssrts/sketch.js
+++ mainssrts/sketch.js
@@ -1,7 +1,17 @@
+var platforms = [];
+var platformCount = 20;
+
 function setup() {
   createCanvas(1200,800);
+  
+  for (var i = 0; i < platformCount; i++) {
+    platforms[i] = new Platform();
+  }
 }
 
 function draw() {
   background(0);
+  for (var i = 0; i < platformCount; i++) {
+    platforms[i].display();
+  }
 }
