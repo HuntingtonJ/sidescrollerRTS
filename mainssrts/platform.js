65b025ce0692963fc27bb62e28e4d098afd3d7ef
Huntington Shin Jasperson <huntingtonjasperson@gmail.com>
2017-06-25 14:54:03 -0700

    changed platform random values

--- mainssrts/platform.js
+++ mainssrts/platform.js
@@ -2,14 +2,14 @@
 function Platform() {
   this.x = random(6000);
   this.y = random(800);
-  this.wid = 40;
-  this.len = random(40,200);
+  this.thickness = 40;
+  this.len = random(100,400);
   
   this.display = function() {
     if (this.x <= 1200) {
       noStroke();
       fill(255);
-      rect(this.x,this.y,this.len,this.wid);
+      rect(this.x,this.y,this.len,this.thickness);
     } 
     
   }
